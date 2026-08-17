071a1bb96e76bc75e4b4c1ec3d857815502ff700
MarchPhantasia <1162030037@qq.com>
2026-08-18 02:46:05 +0800

    feat: finish follow-up workspace polish

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "shici-v30";
-const FILES = ["/", "/styles.css?v=27", "/app.js?v=30", "/vendor/lucide.min.js?v=19", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "shici-v31";
+const FILES = ["/", "/styles.css?v=28", "/app.js?v=31", "/vendor/lucide.min.js?v=19", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
